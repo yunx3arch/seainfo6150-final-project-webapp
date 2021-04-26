e89414e44d69ff207c9ee1750b9f25e043d5b0c1
yunx3arch <64882509+yunx3arch@users.noreply.github.com>
2021-04-26 09:27:15 +0800

    Delete src/Bar directory

--- src/Bar/Bar.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Bar from "./Bar";
-
-describe("Bar tests", () => {
-  it("renders correctly", () => {
-    const { container } = render(<Bar categoryId={0} productId="baz" />);
-    expect(container).toMatchSnapshot();
-  });
-});
